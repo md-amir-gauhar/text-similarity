20e123b6f9ca8d53b08d37da6f8e03bcec6f573a
md-amir-gauhar <gauharamir15@gmail.com>
2023-05-25 15:20:30 +0530

    initlal commit

---
+++ src/app/components/ui/LargeHeading.tsx
@@ -0,0 +1,37 @@
+import React, { HTMLAttributes, forwardRef } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "@/app/lib/utils";
+
+const variants = cva(
+  "text-black darl:text-white text-center lg:text-left font-extrabold leading-tight tracking-tighter",
+  {
+    variants: {
+      size: {
+        default: "text-4xl md:text-5xl lg:text-6xl",
+        lg: "text-5xl md:text-6xl lg:text-7xl",
+        sm: "text-2xl md:text-3xl lg:text-4xl",
+      },
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  }
+);
+
+interface LargeHeadingProps
+  extends HTMLAttributes<HTMLHeadingElement>,
+    VariantProps<typeof variants> {}
+
+const LargeHeading = forwardRef<HTMLHeadingElement, LargeHeadingProps>(
+  ({ className, size, children, ...props }, ref) => {
+    return (
+      <h1 ref={ref} {...props} className={cn(variants({ size, className }))}>
+        {children}
+      </h1>
+    );
+  }
+);
+
+LargeHeading.displayName = "LargeHeading";
+
+export default LargeHeading;
