d1d1606b0d86b5e50f42f7f633576f3a388e6d32
md-amir-gauhar <gauharamir15@gmail.com>
2023-05-28 23:09:34 +0530

    created button component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { getServerSession } from "next-auth";
 import Link from "next/link";
 import React, { FC } from "react";
+import { buttonVariants } from "./ui/Button";
 
 interface NavbarProps {}
 
@@ -9,7 +10,7 @@ const Navbar: FC<NavbarProps> = () => {
   return (
     <div className="fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900 top-0 left-0 right-0 z-50 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between">
       <div className="container max-w-7xl mx-auto w-full flex justify-between items-center">
-        <Link href={"/"} className={buttonVariants({ variants: "link" })}>
+        <Link href={"/"} className={buttonVariants({ variant: "link" })}>
           Text Similarity 1.0
         </Link>
       </div>
@@ -21,7 +22,7 @@ const Navbar: FC<NavbarProps> = () => {
         <ThemeToggle />
         <Link
           href={"/documentation"}
-          className={buttonVariants({ variants: "ghost" })}
+          className={buttonVariants({ variant: "ghost" })}
         >
           Documentation
         </Link>
@@ -31,7 +32,7 @@ const Navbar: FC<NavbarProps> = () => {
             {" "}
             <Link
               href={"/dashboard"}
-              className={buttonVariants({ variants: "ghost" })}
+              className={buttonVariants({ variant: "ghost" })}
             >
               Dashboard
             </Link>
